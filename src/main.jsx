1eb765c51b4341e8155477447fcd4cb474727271
Pavan-Ongolu-Dista <pavan.ongolu@distacart.com>
2026-05-06 00:14:26 +0530

    Add Plain Team to customer list

--- src/main.jsx
+++ src/main.jsx
@@ -50,7 +50,8 @@ const customerInfo = {
   '9138324275479': { name: '@3120 Team'},
   '9161889743127': { name: 'Llamaindex Team', email: 'llamaindex@veeryoffices.com'},
   '9253797691671': { name: 'AYR Energy Team', email: 'ayrenergy@veeryoffices.com'},
-  '9253770690839': { name: 'Lemon slice Team', email: 'lemonslice@veeryoffices.com'}
+  '9253770690839': { name: 'Lemon slice Team', email: 'lemonslice@veeryoffices.com'},
+  '9354786537751': { name: 'Plain Team', email: 'plain@veeryoffices.com'}
 };
 
 function useCustomers() {
